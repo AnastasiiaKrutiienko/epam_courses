08f23cf2ea6f83021ea3032d172b2bd923ced960
Rostyslav Kravchenko <rostyslav_kravchenko@epam.com>
2019-12-24 16:01:07 +0200

    Initialize the repo

---
+++ src/com/epam/lesson1/Lesson1.java
@@ -0,0 +1,14 @@
+package com.epam.lesson1;
+
+/**
+ * Created by Anastasiia 24.12.19
+ */
+public class Lesson1 {
+    public static void main(String[] arg) {
+        System.out.println("* * * *   * * * *      *       *       *");
+        System.out.println("*         *     *     * *      * *   * *");
+        System.out.println("* *       * * * *    *   *     *   *   *");
+        System.out.println("*         *         * * * *    *       *");
+        System.out.println("* * * *   *        *       *   *       *");
+    }
+}
